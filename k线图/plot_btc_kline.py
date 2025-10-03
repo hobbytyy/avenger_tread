a6f8736f02dfd703be96047de71b59574c2a00cb
qiangge <qiangge@example.com>
2025-10-03 22:40:38 +0800

    11

---
+++ k线图/plot_btc_kline.py
@@ -0,0 +1,18 @@
+import finplot as fplt
+import pandas as pd
+
+df = pd.read_csv('/Users/mac/Documents/QT/数据/btc_data_1d.csv')
+df = df.rename(columns={'交易时间': 'time', '开盘价': 'open', '最高价': 'high', '最低价': 'low', '收盘价': 'close', '成交量': 'volume'})
+df['time'] = pd.to_datetime(df['time'])
+df = df.set_index('time')
+
+ax, ax2, ax3 = fplt.create_plot('BTC Daily K-Line', rows=3)
+fplt.candlestick_ochl(df[['open', 'close', 'high', 'low']], ax=ax)
+fplt.volume_ocv(df[['open', 'close', 'volume']], ax=ax2)
+df['macd'] = df['close'].ewm(span=12).mean() - df['close'].ewm(span=26).mean()
+df['signal'] = df['macd'].ewm(span=9).mean()
+df['hist'] = df['macd'] - df['signal']
+fplt.plot(df['macd'], ax=ax3, legend='MACD')
+fplt.plot(df['signal'], ax=ax3, legend='Signal')
+fplt.volume_ocv(df[['open','close','hist']], ax=ax3, colorfunc=fplt.strength_colorfilter)
+fplt.show()
