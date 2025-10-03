9cd8b02f9aa6942310d38369ccd1d0af0b11c901
qiangge <qiangge@example.com>
2025-10-03 22:49:25 +0800

    可用1

--- k线图/plot_btc_kline.py
+++ k线图/plot_btc_kline.py
@@ -1,6 +1,6 @@
 import finplot as fplt
 import pandas as pd
-
+#可用
 df = pd.read_csv('/Users/mac/Documents/QT/数据/btc_data_1d.csv')
 df = df.rename(columns={'交易时间': 'time', '开盘价': 'open', '最高价': 'high', '最低价': 'low', '收盘价': 'close', '成交量': 'volume'})
 df['time'] = pd.to_datetime(df['time'])
